34d1c58c0808dfe0c2af840c867dc51b9c481ba9
Hà Nguyễn <trongha.dev@gmail.com>
2026-09-15 12:09:38 +0700

    feat: add configuration management module to hbb_common

--- src/config.rs
+++ src/config.rs
@@ -114,8 +114,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["172.16.3.28"];
+pub const RS_PUB_KEY: &str = "0RtoIptoH30Glx75q0DuScwdUgLVwf5oqZ0hgCnInNI=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
